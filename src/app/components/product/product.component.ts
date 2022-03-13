3cf714fae3b41a305a4f73c784db35d29ceb9858
Emre Akıncı <emreakinci696@gmail.com>
2022-03-13 15:38:45 +0300

    17. Gün Sonu

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,22 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+}
+
+from '@angular/core';
+
+import {
+  Product
+}
+
+from 'src/app/models/product';
+
+import {
+  ProductResponseModel
+}
+
+from 'src/app/models/productResponseModel';
+
+import {
+  ProductService
+}
+
+from 'src/app/services/product.service';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
-})
-export class ProductComponent implements OnInit {
-
-  product1 = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 }
-  product2 = { productId: 2, productName: 'Laptop', categoryId: 2, unitPrice: 12999 }
-  product3 = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 5 }
-  product4 = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5 }
-
-  products = [this.product1, this.product2, this.product3, this.product4]
-  
-  constructor() { }
-
-  ngOnInit(): void {
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.css'],
   }
 
+) export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded = false;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(response => {
+        this.products = response.data;
+        this.dataLoaded=true;
+      }
+    )
+  }
 }
